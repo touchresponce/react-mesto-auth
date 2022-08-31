842b20101286a23ed0580eb5d7a6ca1adee1acb9
touchresponce <touchresponce71@gmail.com>
2022-08-31 15:55:32 +0300

    feat: логика бургера, кривая верстка

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,32 @@
 import logo from "../images/logo.svg";
 import { Link, withRouter, useLocation } from "react-router-dom";
+import { useState } from "react";
 
 function Header({ loggedIn, email, onSignOut }) {
   const location = useLocation();
 
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <header className="header">
+    <header className={`header ${isOpen ? "header__visible" : ""}`}>
       <img className="header__logo" src={logo} alt="Лого проекта Mesto" />
       {loggedIn ? (
-        <div className="header__wrapper">
+        <div
+          className={`header__wrapper ${
+            isOpen ? "header__wrapper_visible" : ""
+          }`}
+        >
           <p className="header__email">{email}</p>
           <Link className="header__link" to="/sign-in" onClick={onSignOut}>
             Выйти
           </Link>
         </div>
       ) : (
-        <div className="header__wrapper">
+        <div
+          className={`header__wrapper ${
+            isOpen ? "header__wrapper_visible" : ""
+          }`}
+        >
           {location.pathname === "/sign-up" ? (
             <Link className="header__link" to="/sign-in">
               Войти
@@ -27,7 +38,11 @@ function Header({ loggedIn, email, onSignOut }) {
           )}
         </div>
       )}
-      <button className="header__burger" type="button">
+      <button
+        className={`header__burger ${isOpen ? "header__burger-open" : ""}`}
+        type="button"
+        onClick={() => setOpen(!isOpen)}
+      >
         <span className="header__burger-line"></span>
       </button>
     </header>
